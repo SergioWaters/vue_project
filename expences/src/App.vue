171e692a349315b337d00dbecb83f8fe6bc0671a
Serj_NOTE <mokretsovsa@gmail.com>
2022-03-14 15:15:35 +0300

    made basic files

---
+++ expences/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <AddExpence />
+    <ExpencesList />
+  </div>
+</template>
+
+<script>
+import ExpencesList from "./components/ExpencesList.vue";
+import AddExpence from "./components/AddExpence.vue";
+
+export default {
+  name: "App",
+  components: {
+    ExpencesList,
+    AddExpence,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ expences/src/components/AddExpence.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="add-form">
+    <button @click="formVisibile = !formVisibile">Add new expence</button>
+    <form class="expenceAdd" v-show="formVisibile">
+      <input type="date" placeholder="Date" />
+      <input type="number" placeholder="Amount" />
+      <input type="text" placeholder="Description" />
+      <input type="submit" value="put" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddExpence",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+button {
+  color: #42b983;
+}
+</style>
