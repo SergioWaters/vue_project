0e85ff117b8775b4d95e49e578d5c29352732492
Serj_NOTE <mokretsovsa@gmail.com>
2022-04-21 01:21:23 +0300

    added jest, some minor changes

---
+++ src/components/TestComp.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>The message is: {{ msg }}</div>
+</template>
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+};
+</script>
---
+++ src/test/first.test.js
@@ -0,0 +1,8 @@
+import TestComp from "@/components/TestComp.vue"; //компонент, который будем тестить
+import { mount } from "@vue/test-utils"; // метод, принимающий два парама: компонент, который будем тетстить и опции.пропсы тестирования
+
+describe('test for TestComp', () => {
+  test('test props', () => {
+    const wrapper = mount
+  })
+})
